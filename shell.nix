5da5f2259a67140ae410a9441ffc49af2e1cd1b7
Steve Purcell <steve@sanityinc.com>
2022-09-02 16:39:43 +0200

    Add basic unpinned nix shell config with HLS

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {
+  # Force Rosetta on ARM Macs, to let haskell-language-server work
+  localSystem = if builtins.currentSystem == "aarch64-darwin"
+                then "x86_64-darwin"
+                else builtins.currentSystem; } }:
+
+let
+  ghc = pkgs.ghc.withPackages (p: with p; [ JuicyPixels ]);
+in
+pkgs.mkShell {
+
+  buildInputs = [ ghc pkgs.cabal-install pkgs.haskell-language-server ];
+
+}
