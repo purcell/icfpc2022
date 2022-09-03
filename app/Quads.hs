7cfeaf441e538a5ee35bd0a00b64a425de3501b9
Sjoerd Visscher <sjoerd.visscher@iohk.io>
2022-09-03 17:03:48 +0200

    Look deeper

--- app/Quads.hs
+++ app/Quads.hs
@@ -9,12 +9,12 @@ import Cost
 import Cost (point_cut_cost)
 
 fromImage :: Img -> ISL
-fromImage img = toISL img (0, 0) (imageWidth img, imageHeight img) [0]
+fromImage img = snd $ toISL img (0, 0) (imageWidth img, imageHeight img) [0]
 
 powOf2 :: Int -> Int
 powOf2 w = 2 ^ (floor (log (fromIntegral w :: Double) / log 2) :: Int)
 
-toISL :: Img -> Point -> Point -> BlockId -> ISL
+toISL :: Img -> Point -> Point -> BlockId -> (Integer, ISL)
 toISL img (x0,y0) (x1,y1) blockId =
   let w = x1 - x0
       h = y1 - y0
@@ -25,23 +25,14 @@ toISL img (x0,y0) (x1,y1) blockId =
       tr = region img (x, y) (x1, y1)
       tl = region img (x0, y) (x, y1)
       all = region img (x0, y0) (x1, y1)
+      (blCost, blCode) = toISL img (x0, y0) (x, y) (blockId ++ [0])
+      (brCost, brCode) = toISL img (x, y0) (x1, y) (blockId ++ [1])
+      (trCost, trCode) = toISL img (x, y) (x1, y1) (blockId ++ [2])
+      (tlCost, tlCode) = toISL img (x0, y) (x, y1) (blockId ++ [3])
       fillCost = similarityWithAverage all + imgLineCost img all color_cost
-      pointCutCost
-        = similarityWithAverage bl
-        + similarityWithAverage br
-        + similarityWithAverage tr
-        + similarityWithAverage tl
-        + imgLineCost img all point_cut_cost
-        + imgLineCost img bl color_cost
-        + imgLineCost img br color_cost
-        + imgLineCost img tr color_cost
-        + imgLineCost img tl color_cost
-  in if fillCost < pointCutCost
+      pointCutCost = imgLineCost img all point_cut_cost + blCost + brCost + trCost + tlCost
+  in if length blockId > 4 || fillCost < pointCutCost
     then
-      [Color blockId (averageColour (region img (x0,y0) (x1,y1)))]
+      (fillCost, [Color blockId (averageColour (region img (x0,y0) (x1,y1)))])
     else
-      [PointCut blockId (x, y)]
-      ++ toISL img (x0, y0) (x, y) (blockId ++ [0])
-      ++ toISL img (x, y0) (x1, y) (blockId ++ [1])
-      ++ toISL img (x, y) (x1, y1) (blockId ++ [2])
-      ++ toISL img (x0, y) (x, y1) (blockId ++ [3])
+      (pointCutCost, [PointCut blockId (x, y)] ++ blCode ++ brCode ++ trCode ++ tlCode)
