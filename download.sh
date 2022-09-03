8171c965d795856e587d1db5c45f020a3c1b6ce3
Steve Purcell <steve@sanityinc.com>
2022-09-03 16:09:40 +0200

    Add download script, and re-download 27-30

---
+++ download.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+[ -n "$1" ] || { echo "usage: $0 N ..." >&2; exit 1; } 
+
+for f in $*; do
+   curl https://cdn.robovinci.xyz/imageframes/$f.png > problems/$f.png
+   curl https://cdn.robovinci.xyz/imageframes/$f.initial.json | jq . > problems/$f.json
+done
