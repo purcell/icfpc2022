f9ee376a34f8ff381b20bc4efc2e65bdb317934d
Steve Purcell <steve@sanityinc.com>
2022-09-04 16:57:52 +0200

    Add problems 36-40

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ set -e
 [ -n "$1" ] || { echo "usage: $0 N ..." >&2; exit 1; } 
 
 for f in $*; do
-#   curl https://cdn.robovinci.xyz/imageframes/$f.png > problems/$f.png
+   curl https://cdn.robovinci.xyz/imageframes/$f.png > problems/$f.png
    curl https://cdn.robovinci.xyz/imageframes/$f.initial.png > problems/$f.initial.png
-#   curl https://cdn.robovinci.xyz/imageframes/$f.initial.json | jq . > problems/$f.json
+   curl https://cdn.robovinci.xyz/imageframes/$f.initial.json | jq . > problems/$f.json
 done
